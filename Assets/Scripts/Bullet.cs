1bdbfa49ebd4cbc92f521ae11783ae2424263642
AtshshiMori <atsmr.30autb@gmail.com>
2019-10-25 22:22:07 +0900

    first commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log(other.gameObject.tag);
+        if (other.gameObject.tag != "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        if (other.tag == "Enemy")
+        {
+            Rigidbody rg_enemy = other.GetComponent<Rigidbody>();
+            Rigidbody rg_bullet = this.GetComponent<Rigidbody>();
+            rg_enemy.AddForce(rg_bullet.velocity.normalized, ForceMode.Impulse);
+
+            Enemy enemy = other.gameObject.GetComponent<Enemy>();
+            enemy.damaged(10);
+        }
+
+        if (other.tag != "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+
+    public GameObject target;
+    public float slowDistance;
+    public float slowRate;
+
+    NavMeshAgent agent;
+    Animator animator;
+    float life;
+
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+        life = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = target.transform.position;
+
+        if (life < 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void damaged(float damage)
+    {
+        life -= damage;
+    }
+}
